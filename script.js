bfcac5d20b33e870b1c766100d3aa41fdf6e2e9b
mdpais <mdpais@gmail.com>
2022-11-29 02:07:30 -0400

    Completed today's weather

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 var cityInput = document.getElementById("search");
 var searchButton = document.querySelector(".search-btn");
+var weatherToday = document.querySelector(".weather-today");
+var todayContent = document.querySelector(".today-content");
+var weatherFiveDay = document.querySelector(".weather-5day");
 
 var lat;
 var lon;
 
 function getWeather(lat, lon) {
-    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=d1c747d37a69b86b9558f11ef1f6d753";
+    var requestUrlToday = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid=d1c747d37a69b86b9558f11ef1f6d753";
     console.log(lat);
     console.log(lon);
-    fetch(requestUrl)
+    todayContent.textContent = "";
+    fetch(requestUrlToday)
         .then(function(response) {
         return response.json();
         })
         .then(function(data) {
-        console.log(data)
+        console.log(data);
+        var todayDate = document.createElement("h5");
+        var todayTemp = document.createElement("p");
+        var todayWind = document.createElement("p");
+        var todayHumidity = document.createElement("p");
+        todayDate.textContent = data.name + ", " + data.sys.country + " (" + dayjs().format("D MMMM YYYY") + ")";
+        todayTemp.textContent = "Temperature: " + data.main.temp + "°C";
+        todayWind.textContent = "Wind Speed: " + data.wind.speed + "kmph";
+        todayHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+        todayDate.setAttribute("class", "card-title");
+        todayTemp.setAttribute("class", "card-text");
+        todayWind.setAttribute("class", "card-text");
+        todayHumidity.setAttribute("class", "card-text");
+        todayContent.appendChild(todayDate);
+        todayContent.appendChild(todayTemp);
+        todayContent.appendChild(todayWind);
+        todayContent.appendChild(todayHumidity);
+        weatherToday.style.display = "block";
         });
+
 }
 
 function getLatLon() {
