dbf07b083b139e8ceb16e7e9b58279cb719d3d96
mdpais <mdpais@gmail.com>
2022-11-29 02:55:42 -0400

    Added weather icon and corrected the API

--- script.js
+++ script.js
@@ -8,35 +8,39 @@ var lat;
 var lon;
 
 function getWeather(lat, lon) {
-    var requestUrlToday = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid=d1c747d37a69b86b9558f11ef1f6d753";
+    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=metric&cnt=6&appid=d1c747d37a69b86b9558f11ef1f6d753";
     console.log(lat);
     console.log(lon);
     todayContent.textContent = "";
-    fetch(requestUrlToday)
+    fetch(requestUrl)
         .then(function(response) {
-        return response.json();
+            return response.json();
         })
         .then(function(data) {
-        console.log(data);
-        var todayDate = document.createElement("h5");
-        var todayTemp = document.createElement("p");
-        var todayWind = document.createElement("p");
-        var todayHumidity = document.createElement("p");
-        todayDate.textContent = data.name + ", " + data.sys.country + " (" + dayjs().format("D MMMM YYYY") + ")";
-        todayTemp.textContent = "Temperature: " + data.main.temp + "°C";
-        todayWind.textContent = "Wind Speed: " + data.wind.speed + "kmph";
-        todayHumidity.textContent = "Humidity: " + data.main.humidity + "%";
-        todayDate.setAttribute("class", "card-title");
-        todayTemp.setAttribute("class", "card-text");
-        todayWind.setAttribute("class", "card-text");
-        todayHumidity.setAttribute("class", "card-text");
-        todayContent.appendChild(todayDate);
-        todayContent.appendChild(todayTemp);
-        todayContent.appendChild(todayWind);
-        todayContent.appendChild(todayHumidity);
-        weatherToday.style.display = "block";
+            console.log(data.list[0].weather[0].icon);
+            var todayDate = document.createElement("h5");
+            var todayTemp = document.createElement("p");
+            var todayWind = document.createElement("p");
+            var todayHumidity = document.createElement("p");
+            var todayImage = document.createElement("img");
+            todayDate.textContent = data.city.name + ", " + data.city.country + " (" + dayjs().format("D MMMM YYYY") + ")";
+            todayTemp.textContent = "Temperature: " + data.list[0].main.temp + "°C";
+            todayWind.textContent = "Wind Speed: " + data.list[0].wind.speed + " kmph";
+            todayHumidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+            todayImage.setAttribute("src", "https://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png");
+            todayDate.setAttribute("class", "card-title");
+            todayTemp.setAttribute("class", "card-text");
+            todayWind.setAttribute("class", "card-text");
+            todayHumidity.setAttribute("class", "card-text");
+            todayContent.appendChild(todayDate);
+            todayDate.appendChild(todayImage);
+            todayContent.appendChild(todayTemp);
+            todayContent.appendChild(todayWind);
+            todayContent.appendChild(todayHumidity);
+            weatherToday.style.display = "block";
+            weatherFiveDay.style.display = "flex";
+            cityInput.value = "";
         });
-
 }
 
 function getLatLon() {
