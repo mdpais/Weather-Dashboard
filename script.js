f9adffbe1388d1d3632be108e5d4bc357bcd0586
mdpais <mdpais@gmail.com>
2022-11-28 23:33:37 -0400

    Wrote function to get latitude and longitude from the city and pass it to the getWeather function

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ var searchButton = document.querySelector(".search-btn");
 var lat;
 var lon;
 
-function getWeather() {
-    var requestUrl = "api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=d1c747d37a69b86b9558f11ef1f6d753";
-    console.log("weather");
+function getWeather(lat, lon) {
+    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=d1c747d37a69b86b9558f11ef1f6d753";
+    console.log(lat);
+    console.log(lon);
 }
 
 function getLatLon() {
-    var requestUrl = "api.openweathermap.org/data/2.5/forecast?q="+cityInput.value+"&appid=d1c747d37a69b86b9558f11ef1f6d753";
+    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+cityInput.value+"&appid=d1c747d37a69b86b9558f11ef1f6d753";
     console.log(cityInput.value);
-        
-        getWeather(lat,lon);
+    fetch(requestUrl)
+        .then(function(response) {
+            if (response.status === 404) {
+                throw new Error();
+            }    
+            return response.json();        })
+        .then(function(data) {
+            console.log("reached here");
+            lat = data.city.coord.lat;
+            lon = data.city.coord.lon;
+            getWeather(lat, lon);
+            //add code to store city in local storage
+        })    
+        .catch(function(error) {
+            return;
+        });
 }
 
 searchButton.addEventListener("click", getLatLon);
